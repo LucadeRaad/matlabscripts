11ce506cebb46de39b11025180a4bb280432a31e
LucadeRaad <lucaderaad@outlook.com>
2022-10-27 20:52:29 -0700

    gabor transform

--- parse_jaw_clench_data/main.m
+++ parse_jaw_clench_data/main.m
@@ -13,11 +13,11 @@ filtered_unordered_eeg = parse_eeg('Data_RandomClench', '*_filtered.csv');
 
 raw_ordered_eeg = parse_eeg('Data_Clench_RAW', '*_filtered.csv');
 
-%% Gabor transformation
+%% Gabor transform
 % Create a sliding window, do fft(fast fourier transformation) on every
 % sliding window, record maybe the mode or slice the fft graph into pieces
 % and then recording the pieces
 
-graph_gabor(filtered_unordered_eeg(3), 900, 450, 1);
+graph_gabor(filtered_unordered_eeg(3), 900, 0, 1);
 
 %graph_gabor(raw_ordered_eeg);
