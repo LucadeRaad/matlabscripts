04a109759f839de536de712651874f71a9e8add9
LucadeRaad <lucaderaad@outlook.com>
2022-12-15 01:43:04 -0800

    legend x and y axises

--- parse_jaw_clench_data/realtime_gabor.m
+++ parse_jaw_clench_data/realtime_gabor.m
@@ -7,11 +7,17 @@ t = tcpip('localhost', 8844);
 fclose(t); %just in case it was open from a previous iteration
 fopen(t); %opens the TCPIP connection
 
-headerStart = [64, 65, 66, 67, 68]; % All DSI packet headers begin with '@ABCD', corresponding to these ASCII codes.
-cutoffcounter = 0;
-notDone = 1;
+newcolors = [0.83 0.14 0.14
+             1.00 0.54 0.00
+             0.47 0.25 0.80
+             0.25 0.80 0.54
+             0.00 0.00 1.00];
 
-graph_write_delay = 36;
+colororder(newcolors);
+
+% Don't ever change this, unless wearable sensing changes their packet
+% structure
+HEADER_START = [64, 65, 66, 67, 68]; % All DSI packet headers begin with '@ABCD', corresponding to these ASCII codes.
 
 %% A bunch of constants
 % These constants are set up so that a future user can change them for
@@ -26,15 +32,13 @@ FIGURE_Y_MAX_MID_LIM = FIGURE_Y_MAX_LIM / 2;
 FIGURE_Y_MIN_LIM = -1 * FIGURE_Y_MAX_LIM;
 FIGURE_Y_MIN_MID_LIM = FIGURE_Y_MIN_LIM / 2;
 
-%BANDPASS_RANGE = [1, 50];
+FIGURE_WRITE_DELAY = 36;
 
 SLICE_COUNT = 9;
 
 SLICE_STEP = 5;
 
 DATAPOINTS_PER_SEC = 300;
-
-BANDPASS_RANGE = [1, 50];
 
 DATABUFFER_SIZE = 1201;
 
@@ -49,12 +53,14 @@ dt = 1/Fs;
 FREQ_INCR = 1 / (dt * GABOR_WINDOW_SIZE);
 
 % https://stackoverflow.com/questions/31325491/how-to-design-a-zero-phase-bandpass-filter-for-1-to-20-hz-in-matlab
-B_PASS = fir1(400,[1,50]/(Fs/2));
+% Changed to be a 1-50 bandpass
+B_PASS = fir1(400, [1,50] / (Fs / 2));
 A_PASS = 1;
 
 gaborCount = 0;
 dataCount = 0;
 plotCounter = 0;
+packetDropCounter = 0;
 
 % Initialize timeLog and plotCounter for real time plotting
 allData = zeros(DATABUFFER_SIZE, NUM_CHANNELS);
@@ -64,29 +70,48 @@ algOutput = zeros(DATABUFFER_SIZE, 1);
 
 sliceVals = zeros(SLICE_COUNT, NUM_CHANNELS);
 
-%pause
+%% Figure Initialization
+
+title('Realtime EEG Processing','HandleVisibility','off');
+ylabel('Microvolts','HandleVisibility','off');
+xlabel('Data Points (300 samples per second)','HandleVisibility','off');
+
+hold on
+
+ylim([FIGURE_Y_MIN_LIM, FIGURE_Y_MAX_LIM])
+
+% https://www.mathworks.com/matlabcentral/answers/338733-how-to-stop-legend-from-adding-data1-data2-when-additional-data-is-plotted
+set(0, 'DefaultLegendAutoUpdate', 'off')
+
+legend('F4-LE', 'C4-LE', 'P3-LE', 'p4-LE', 'Algorithm Output', 'Location', 'northwest');
+
+notDone = 1;
 
 while notDone
     %% Termination clause
-    if t.Bytesavailable < 12                     %if there's not even enough data available to read the header
-        cutoffcounter = cutoffcounter + 1;       %take a step towards terminating the whole thing
-        if cutoffcounter == MAX_PACKETS_DROPPED  %and if 1500 steps go by without any new data,
-            notDone = 0;                         %terminate the loop.
+    if t.Bytesavailable < 12                        %if there's not even enough data available to read the header
+        packetDropCounter = packetDropCounter + 1;  %take a step towards terminating the whole thing
+
+        if packetDropCounter == MAX_PACKETS_DROPPED %and if 1500 steps go by without any new data,
+            notDone = 0;                            %terminate the loop.
         end
+
         disp('no bytes available') % Load bearing disp(). If removed increase the pause time
         pause(0.001)
         continue
     else  %meaning, unless there's data available.
-        cutoffcounter = 0;
+        packetDropCounter = 0;
     end
 
-    % Read the packet
+    %% Read the packet
     packet_info = uint8(fread(t, 12))'; % Loads the first 12 bytes of the first packet, which should be the header
     data = [packet_info, uint8(fread(t, double(typecast(fliplr(packet_info(7:8)), 'uint16'))))']; % Loads the full packet, based on the header
     lengthdata = length(data);
 
-    if all(ismember(headerStart,data)) % Checks if the packet contains the header
-        packetType = data(6); %this determines whether it's an event or sensor packet.
+    % Checks if the packet contains the header
+    if all(ismember(HEADER_START, data)) 
+        % This determines whether it's an event or sensor packet.
+        packetType = data(6);          
 
         %% Event Packet.  This includes the greeting packet
         if packetType == 5
@@ -102,6 +127,7 @@ while notDone
 
             % We discard EEG channels that are unused in the algorithm.
             % we only use:
+
             % Channel 2: F4-LE
             % Channel 4: C4-LE
             % Channel 6: P3-LE
@@ -111,7 +137,7 @@ while notDone
             EEGdata(3) = [];
             EEGdata(1) = [];
 
-            %% Plot data in realtime
+            %% Add EEG data to buffer
             if dataCount <= DATABUFFER_SIZE
                 dataCount = dataCount + 1;
 
@@ -135,27 +161,26 @@ while notDone
                 algOutput(end) = 0;
             end
             
+            %% Perform gabor transform on data
             if (gaborCount >= GABOR_WINDOW_SIZE)
-                gaborSlice = filtfilt(B_PASS, A_PASS, allData);%bandpass(allData, BANDPASS_RANGE, DATAPOINTS_PER_SEC);
+                displayData = filtfilt(B_PASS, A_PASS, allData);
 
-                displayData = gaborSlice;
-
-                gaborSlice = gaborSlice(dataCount - GABOR_WINDOW_SIZE:dataCount, :);
+                gaborSlice = displayData(dataCount - GABOR_WINDOW_SIZE:dataCount, :);
 
                 fhat = fft(gaborSlice, [], 1);
                 PSD = fhat.*conj(fhat) / GABOR_WINDOW_SIZE;
 
-                for k = 5:SLICE_STEP:(SLICE_STEP * (SLICE_COUNT))
+                for k = SLICE_STEP:SLICE_STEP:(SLICE_STEP * SLICE_COUNT)
                     s_ind = floor(k / FREQ_INCR);
                     e_ind = floor((k + SLICE_STEP) / FREQ_INCR) + 1;
-                
-                    sliceVals(k / SLICE_STEP, :) = mean(PSD(s_ind : e_ind, :));
-                end
-       
-                [~, mininds] = min(sliceVals, [], 1);
-                [~, maxinds] = max(sliceVals, [], 1);
 
-                allChannels = maxinds > mininds;
+                    sliceVals(k / SLICE_STEP, :) = mean(PSD(s_ind:e_ind, :));
+                end
+
+                [~, minInds] = min(sliceVals, [], 1);
+                [~, maxInds] = max(sliceVals, [], 1);
+
+                allChannels = maxInds > minInds;
 
                 isClench = allChannels(1) | allChannels(2) | allChannels(3) | allChannels(4);
 
@@ -165,30 +190,36 @@ while notDone
                     algOutput(dataCount - GABOR_WINDOW_SIZE:dataCount, :) = FIGURE_Y_MIN_MID_LIM;
                 end
                 
-
                 gaborCount = 0;
             else
                 gaborCount = gaborCount + 1;
             end
 
-            if plotCounter >= graph_write_delay % MATLAB waits a specified number of cycles before plotting the data to increase performance.
+            %% Plot data in realtime
+            if plotCounter >= FIGURE_WRITE_DELAY % MATLAB waits a specified number of cycles before plotting the data to increase performance.
                 if dataCount <= FIGURE_SIZE
-                    smallSlice = 1 : dataCount;
+                    smallSlice = 1:dataCount;
 
                     graphLog = [displayData(smallSlice, :), algOutput(smallSlice, :)];
                     graphTime = timeLog(smallSlice);
                 else
                     bigSlice = dataCount - FIGURE_SIZE;
 
-                    graphLog = [displayData(bigSlice: dataCount, :), algOutput(bigSlice: dataCount)];
-                    graphTime = timeLog(bigSlice: dataCount);
+                    graphLog = [displayData(bigSlice:dataCount, :), algOutput(bigSlice:dataCount)];
+                    graphTime = timeLog(bigSlice:dataCount);
                 end
-  
-                plot(graphTime, graphLog)
+
+                set(gca,'NextPlot','replacechildren');
+
+                plot(graphTime, graphLog);
+
+                legend show
 
                 xlim([graphTime(1) - 1 / DATAPOINTS_PER_SEC, graphTime(end) + 1 / DATAPOINTS_PER_SEC])
 
-                ylim([FIGURE_Y_MIN_LIM, FIGURE_Y_MAX_LIM])
+                %legend('F4-LE', 'C4-LE', 'P3-LE', 'F4-LE', 'Algorithm Output')
+
+                
                 
                 drawnow;
 
